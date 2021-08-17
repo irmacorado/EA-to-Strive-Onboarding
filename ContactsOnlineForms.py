23d8ea29eab616643d6f486703d56bb7d7322f46
irmacorado <65259720+irmacorado@users.noreply.github.com>
2021-08-17 10:26:47 -0400

    Update ContactsOnlineForms.py

--- ContactsOnlineForms.py
+++ ContactsOnlineForms.py
@@ -43,7 +43,7 @@ logger.setLevel('INFO')
 
 ## edits start here
 
-url = "https://api.securevan.com/v4/changedEntityExportJobs/fields/ContactsOnlineForms"
+url = "https://api.securevan.com/v4/changedEntityExportJobs/fields"
 
 headers = {"Accept": "application/json"}
 
